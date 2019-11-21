236181db9bfd9f8591ca1ca482aef330789fa13a
toff <zehnacker.c@gmail.com>
2019-11-21 21:33:14 +0100

    test(dollar): first test green

--- src/BusinessRules/Entity/Money/Dollar.php
+++ src/BusinessRules/Entity/Money/Dollar.php
@@ -1,8 +1,17 @@
 <?php
 
-namespace Businessrules\Entity\Money;
+declare(strict_types=1);
+
+namespace BusinessRules\Entity\Money;
 
 class Dollar
 {
+    /**
+     * @var int
+     */
+    public $amount = 10;
 
+    public function times(){
+
+    }
 }
--- tests/BusinessRules/Entity/Money/DollarTest.php
+++ tests/BusinessRules/Entity/Money/DollarTest.php
@@ -1,14 +1,18 @@
 <?php
 
-namespace Businessrules\Entity\Money;
+declare(strict_types=1);
 
+namespace tests\BusinessRules\Entity\Money;
 
+use BusinessRules\Entity\Money\Dollar;
 use PHPUnit\Framework\TestCase;
 
 class DollarTest extends TestCase
 {
-    final public function testMultiplication():void
+    final public function testMultiplication(): void
     {
-        $this->fail();
+        $fiveDollars = new Dollar(5);
+        $fiveDollars->times(2);
+        $this->assertEquals(10, $fiveDollars->amount);
     }
 }
