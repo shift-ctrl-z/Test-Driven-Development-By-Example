b20de43c6e7377f5ad4df36875d71e5b6b6ddf13
toff <zehnacker.c@gmail.com>
2019-11-22 08:27:54 +0100

    wip(money): chapter 11, the root of all evil

--- tests/BusinessRules/Entity/Money/DollarTest.php
+++ tests/BusinessRules/Entity/Money/DollarTest.php
@@ -5,14 +5,28 @@ declare(strict_types=1);
 namespace tests\BusinessRules\Entity\Money;
 
 use BusinessRules\Entity\Money\Dollar;
+use BusinessRules\Entity\Money\Franc;
+use BusinessRules\Entity\Money\Money;
 use PHPUnit\Framework\TestCase;
 
 class DollarTest extends TestCase
 {
-    final public function testMultiplication(): void
+    final public function testDollarMultiplications(): void
     {
-        $fiveDollars = new Dollar(5);
-        $fiveDollars->times(2);
-        $this->assertEquals(10, $fiveDollars->amount);
+        $this->assertTrue(Money::dollar(10)->equals(Money::dollar(5)->times(2)));
+        $this->assertTrue(Money::dollar(15)->equals(Money::dollar(5)->times(3)));
+    }
+
+    final public function testEquality(): void
+    {
+        $this->assertTrue((Money::dollar(5))->equals(Money::dollar(5)));
+        $this->assertFalse((Money::dollar(5))->equals(Money::dollar(6)));
+        $this->assertFalse((Money::franc(5))->equals(Money::dollar(5)));
+    }
+
+    final public function testCurrency(): void
+    {
+        $this->assertEquals('USD', Money::dollar(1)->getCurrency());
+        $this->assertEquals('CHF', Money::franc(1)->getCurrency());
     }
 }
